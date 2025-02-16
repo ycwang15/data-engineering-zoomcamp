8b281d868099add2dc0642b92b9cc6f30c894b42
Yuchen Wang <ywang32@mail.yu.edu>
2025-02-16 22:40:57 +0000

    init my dbt project!

--- 04-analytics-engineering/taxi_rides_ny_yuchen/models/example/my_first_dbt_model.sql
+++ 04-analytics-engineering/taxi_rides_ny_yuchen/models/example/my_first_dbt_model.sql
@@ -4,7 +4,7 @@
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
 
-    Try changing "table" to "view" below
+    Try changing "table" to "view" below.
 */
 
 {{ config(materialized='table') }}
